3b06f8368e302e58885f447e0aa7408787b1814f
brian <briankathukya2000@gmail.com>
2020-12-03 18:59:33 +0300

    Changed data source props in app.properties

--- src/main/java/tech/kitucode/Learn/service/CustomerService.java
+++ src/main/java/tech/kitucode/Learn/service/CustomerService.java
@@ -1,15 +1,18 @@
 package tech.kitucode.Learn.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import tech.kitucode.Learn.domain.Customer;
 import tech.kitucode.Learn.repository.CustomerRepository;
 import tech.kitucode.Learn.service.dto.CustomerDTO;
 import tech.kitucode.Learn.service.mapper.CustomerMapper;
 
-import java.util.List;
 
 @Service
 public class CustomerService {
+    private final Logger logger = LoggerFactory.getLogger(CustomerService.class);
+
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
 
@@ -20,6 +23,9 @@ public class CustomerService {
     }
 
     public CustomerDTO save(CustomerDTO customerDTO){
+
+        logger.debug("Request to save customer : {}",customerDTO);
+
         // convert dto to entity for saving
         Customer customer = customerMapper.toEntity(customerDTO);
 
